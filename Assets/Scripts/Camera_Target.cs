e2eb2d7711453462197b426de2817d90d9ddeefc
naimad-adrec <106353663+naimad-adrec@users.noreply.github.com>
2023-02-12 22:08:22 -0800

    Update Movement and Start AI

---
+++ Assets/Scripts/Camera_Target.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Target : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform zTrans;
+    [SerializeField] private float threshold;
+    public Vector3 mousePos;
+
+    void Update()
+    {
+        
+        Vector3 targetPos = (zTrans.position + mousePos) / 2f;
+
+        targetPos.x = Mathf.Clamp(targetPos.x, -threshold + zTrans.position.x, threshold + zTrans.position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, -threshold + zTrans.position.y, threshold + zTrans.position.y);
+
+        this.transform.position = targetPos;
+    }
+
+}
